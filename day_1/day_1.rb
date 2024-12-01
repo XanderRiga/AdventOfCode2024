16c682a0e54c3016d4503455f868d986369d7adc
Xander Riga <xanderriga@live.com>
2024-12-01 08:48:08 -0800

    Day 1 - Part 2

--- day_1/day_1.rb
+++ day_1/day_1.rb
@@ -19,15 +19,22 @@ def distance(left, right)
   (left - right).abs
 end
 
+def similarity_score(left_num, right_tally)
+  left_num * right_tally.fetch(left_num, 0)
+end
+
 def main
   left, right = generate_lists
 
   distances = []
+  similarity_scores = []
   left.length.times do |index|
     distances << distance(left[index], right[index])
+    similarity_scores << similarity_score(left[index], right.tally)
   end
 
   puts "Sum of Distances: #{distances.sum}"
+  puts "Sum of Similarity Scores: #{similarity_scores.sum}"
 end
 
 main
