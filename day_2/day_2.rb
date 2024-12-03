71ba0098c60cfb91e0d1b225da694960069accc5
Xander Riga <xanderriga@live.com>
2024-12-02 19:23:42 -0800

    Day 2 - Part 2
    I feel dumb for this one. I got myself confused between the changes
    array and report array and did some weirdness that made this take
    forever. I will leave this here un-refactored as a sign of my shame.

--- day_2/day_2.rb
+++ day_2/day_2.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 def parse_input
   reports = []
 
@@ -21,16 +23,65 @@ def build_changes(report)
   changes
 end
 
+def is_p1_safe?(changes)
+  return true if (changes.all? { |ch| ch.positive? } || changes.all? { |ch| ch.negative? }) &&
+    changes.all? { |ch| ch.abs >= 1 && ch.abs <= 3 }
+
+  false
+end
+
+def first_problem_index(changes, report)
+  changes.each_with_index do |v, i|
+    prev_changes = changes[0...i]
+
+    return i if v.abs > 3 || v.abs < 1
+
+    next if prev_changes == []
+
+    if prev_changes.all?(&:positive?)
+      return i if v.negative?
+    elsif prev_changes.all?(&:negative?)
+      return i if v.positive?
+    end
+  end
+end
+
 def is_safe?(report)
   changes = build_changes(report)
 
-  # Check for all increasing or all decreasing
-  return false unless changes.all? { |ch| ch.positive? } || changes.all? { |ch| ch.negative? }
+  if is_p1_safe?(changes)
+    puts "Safe with no changes: report: #{report}, changes: #{changes}"
+    return true
+  end
 
-  # Check all changes are within 1-3 difference
-  return false unless changes.all? { |ch| ch.abs >= 1 && ch.abs <= 3 }
+  index = first_problem_index(changes, report)
 
-  true
+  report_1 = report.dup
+  report_2 = report.dup
+  report_3 = report.dup
+
+  report_1.delete_at(index)
+  report_2.delete_at(index + 1)
+  report_3.delete_at(index - 1)
+  changes_1 = build_changes(report_1)
+  changes_2 = build_changes(report_2)
+  changes_3 = build_changes(report_3)
+
+  if is_p1_safe?(changes_1)
+    puts "Changes 1 SAFE: report: #{report}, changes: #{changes}, changes_1: #{changes_1}"
+    return true
+  end
+  if is_p1_safe?(changes_2)
+    puts "Changes 2 SAFE: report: #{report}, changes: #{changes}, changes_2: #{changes_2}"
+    return true
+  end
+  if is_p1_safe?(changes_3)
+    puts "Changes 3 SAFE: report: #{report}, changes: #{changes}, changes_3: #{changes_3}"
+    return true
+  end
+
+  puts "UNSAFE: report: #{report}, changes: #{changes}"
+  false
 end
 
 def main
